6fd86bc291835521843b069cb4a590586a43b0fd
marliyehez <74520448+marliyehez@users.noreply.github.com>
2023-06-30 12:30:36 +0700

    Update config.py

--- project/config.py
+++ project/config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI =  'sqlite:///db.sqlite3' #'postgresql://myuser:mypassword@db/mydatabase'
+SQLALCHEMY_DATABASE_URI =  'postgresql://myuser:mypassword@db/mydatabase' #'sqlite:///db.sqlite3'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'this is secret'
