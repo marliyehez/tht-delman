8147b521c3a8087bfd5f55a6a5da5e43ccf06972
marliyehez <74520448+marliyehez@users.noreply.github.com>
2023-07-31 13:13:43 +0700

    Update model.py

--- project/model.py
+++ project/model.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 from enum import Enum
-from functools import wraps
 import bcrypt
 from datetime import datetime
 
@@ -15,8 +15,7 @@ class StatusEnum(Enum):
 class Login(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
-    password = db.Column(db.String(100), nullable=False)
-    is_active = db.Column(db.Boolean, default=False)
+    password = db.Column(db.String(255), nullable=False)
 
 
 class Employee(db.Model):
@@ -65,58 +64,41 @@ class Appointment (db.Model):
     doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.id'))
     datetm = db.Column(db.DateTime)
     status = db.Column(db.Enum(StatusEnum))
-    diagnose = db.Column(db.String(100))
-    notes = db.Column(db.String(100))
+    diagnose = db.Column(db.String(90))
+    notes = db.Column(db.String(90))
 
     def __repr__(self):
         return f'<Appointment: {self.id}>'
 
 
-def employee_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        num_employee_active = 0
-        active_users = Login.query.filter_by(is_active=True)
-        employees_login_id = [employee.login_id for employee in Employee.query.all()]
-        for active_user in active_users:
-            if active_user.id in employees_login_id:
-                num_employee_active += 1
-                break
-        
-        is_any_employee_active =  num_employee_active > 0
-
-        if is_any_employee_active:
-            return f(*args, **kwargs)
-        return {"message": "There is no active employee at the moment."}
-
-    return decorated
-
 def initialize_data():
+    try :
+        # Add admin
+        admin_data = {"name": "Admin", "username": "admin", "gender": "M", "password": "pass", "birthdate": "2023-06-30"}
+        admin_data['birthdate'] = datetime.fromisoformat(admin_data['birthdate']).date()
+        admin_data['password'] = bcrypt.hashpw(admin_data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        login = Login(username=admin_data['username'], password=admin_data['password'])
+        db.session.add(login)
+        db.session.commit()
+        del admin_data["username"]
+        del admin_data["password"]
+        admin = Employee(**admin_data, login_id=login.id)
+        db.session.add(admin)
+        db.session.commit()
 
-    # Add admin
-    admin_data = {"name": "Admin", "username": "admin", "gender": "M", "password": "pass", "birthdate": "2023-06-30"}
-    admin_data['birthdate'] = datetime.fromisoformat(admin_data['birthdate']).date()
-    admin_data['password'] = bcrypt.hashpw(admin_data['password'].encode('utf-8'), bcrypt.gensalt())
-    login = Login(username=admin_data['username'], password=admin_data['password'])
-    db.session.add(login)
-    db.session.commit()
-    del admin_data["username"]
-    del admin_data["password"]
-    admin = Employee(**admin_data, login_id=login.id)
-    db.session.add(admin)
-    db.session.commit()
-
-    # Add first doctor
-    doctor_data = {"name": "Doctor 1", "username": "doc1", "password": "pw1", "gender": "F", "birthdate": "2000-06-30", "work_start_time":"08:30:00", "work_end_time":"19:30:00"}
-    doctor_data['password'] = bcrypt.hashpw(doctor_data['password'].encode('utf-8'), bcrypt.gensalt())
-    doctor_data['birthdate'] = datetime.fromisoformat(doctor_data['birthdate']).date()
-    doctor_data['work_start_time'] = datetime.strptime(doctor_data['work_start_time'], "%H:%M:%S").time()
-    doctor_data['work_end_time'] = datetime.strptime(doctor_data['work_end_time'], "%H:%M:%S").time()
-    login = Login(username=doctor_data['username'], password=doctor_data['password'])
-    db.session.add(login)
-    db.session.commit()
-    del doctor_data["username"]
-    del doctor_data["password"]
-    doctor = Doctor(**doctor_data, login_id=login.id)
-    db.session.add(doctor)
-    db.session.commit()
+        # Add first doctor
+        doctor_data = {"name": "Doctor 1", "username": "doc1", "password": "pw1", "gender": "F", "birthdate": "2000-06-30", "work_start_time":"08:30:00", "work_end_time":"19:30:00"}
+        doctor_data['password'] = bcrypt.hashpw(doctor_data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        doctor_data['birthdate'] = datetime.fromisoformat(doctor_data['birthdate']).date()
+        doctor_data['work_start_time'] = datetime.strptime(doctor_data['work_start_time'], "%H:%M:%S").time()
+        doctor_data['work_end_time'] = datetime.strptime(doctor_data['work_end_time'], "%H:%M:%S").time()
+        login = Login(username=doctor_data['username'], password=doctor_data['password'])
+        db.session.add(login)
+        db.session.commit()
+        del doctor_data["username"]
+        del doctor_data["password"]
+        doctor = Doctor(**doctor_data, login_id=login.id)
+        db.session.add(doctor)
+        db.session.commit()
+    except IntegrityError:
+        pass
