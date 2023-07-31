72f74950d1c583c889a210927e36abda9f810f64
marliyehez <74520448+marliyehez@users.noreply.github.com>
2023-07-31 13:25:33 +0700

    Update login.py

--- project/resources/login.py
+++ project/resources/login.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request
 from ..model import db, Login
+from ..config import SECRET_KEY
 import bcrypt
+import jwt
+from datetime import datetime, timedelta
 
 
 blp = Blueprint('login', __name__)
@@ -10,10 +13,24 @@ def handling_login():
     auth = request.get_json()
     username, password = auth['username'], auth['password']
     login = Login.query.filter_by(username=username).first()
+
     if login == None:
         return {"message": "Failed. Username is not registered."}
-    if bcrypt.checkpw(password.encode('utf-8'), login.password):
-        login.is_active = True # login_user(login)
-        db.session.commit()
-        return {"message": "Success. Username and password are registered."}
+    
+    if bcrypt.checkpw(password.encode('utf-8'), login.password.encode('utf-8')):
+        iat = datetime.utcnow() + timedelta(hours=7)  # UTC+7
+        exp = iat + timedelta(minutes=30)
+
+        payload = {
+            "sub": f"{username}",
+            "iat": int(iat.timestamp()),
+            "exp": int(exp.timestamp()),
+            "login_id": login.id,
+        }
+        token = jwt.encode(payload, SECRET_KEY)
+        
+        return {
+            "message": "Success. Username and password are registered.",
+            "token": token
+        }
     return {"message": "Failed. Password does not match the username."}
